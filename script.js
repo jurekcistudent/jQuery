0c83b43095f6d4b7f71cdca25a380d6202aef218
jurekcistudent <jurekoor@gmail.com>
2019-09-04 19:50:35 +0000

    Introduction challage and jQuery event challages done

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
 
+    /*
+    // Challange jQuery events 4
+    $("#effect1").mouseenter(function(){
+        $('#effect1').fadeTo('slow', 0.5);
+    });
+    $("#effect1").mouseleave(function(){
+        $('#effect1').fadeTo('slow', 1);
+    });
 
- 	$("#stream1_btn").on("click", function() {
+    // Challange jQuery events 2 and 3
+    $("#effect1").click(function(){
+        $("#par1").slideToggle('2000');
+    });
+    $("#effect2").click(function(){
+        $("#par2").toggle('2000');
+    });
+    $("#effect3").click(function(){
+        $("#par3").toggle('2000');
+    });
+    $("#effect4").click(function(){
+        $("#par4").toggle('2000');
+    });
+    $("#effect5").click(function(){
+        $("#par5").toggle('2000');
+    });
+    $("#effect6").click(function(){
+        $("#par6").toggle('2000');
+    });
+
+    //Challange jQuery events 1
+    $("#effect").click(function(){
+            $('#effect').hide('slow');
+    });
+ 	 //Challange jQuery intro
+     $("#stream1_btn").on("click", function() {
  		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
@@ -79,6 +112,7 @@ $(document).ready(function() {
         $("#hr-django").removeClass("zoom");
         $("#hr-css").addClass("zoom");
     });
+    */
 
 
 });
